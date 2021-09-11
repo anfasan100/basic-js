e926069848a9435d5ba4a3c119fcd35663b70752
anfasan100 <anfasan100@mail.com>
2021-09-11 16:56:58 +0200

    feat: add func count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,7 +14,12 @@ import { NotImplementedError } from '../extensions/index.js';
  * ]) => 3`
  *
  */
-export default function countCats(/* matrix */) {
+export default function countCats(arr) {
   throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  let numOfCats = arr.reduce((catSum,box) => {
+    let cats = box.filter(item => item == '^^');
+    catSum += cats;
+  }, 0);
+  return numOfCats;
 }
+
