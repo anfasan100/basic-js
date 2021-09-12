3ecbb9f5f84d7685647a9e06da39850437c0ae18
anfasan100 <anfasan100@mail.com>
2021-09-12 20:34:43 +0200

    feat: add fuc what-season

--- src/what-season.js
+++ src/what-season.js
@@ -11,7 +11,22 @@ import { NotImplementedError } from '../extensions/index.js';
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-export default function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getSeason(date) {
+  let season = 'Unable to determine the time of year!'
+  if ( date === undefined)  {
+    return season;
+  } else if (!(date instanceof Date) || Object.values(date).length > 0) {
+    throw new Error("Invalid date!");
+  }
+  let month = date.getMonth();
+  if ( month == 11 || month < 2){
+    season = 'winter'
+  } else if ( 2 <= month && month < 5 ){
+    season = 'spring'
+  } else if ( 5 <= month && month < 8 ){
+    season = 'summer'
+  } if ( 8 <= month && month < 11 ){
+    season = 'autumn'
+  } 
+  return season;
 }
